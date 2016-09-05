30a9a5121605f18e3ceeec2263554dd5432c21e5
Quan Dai <qdai@koresystemsgroup.com>
2016-09-05 00:28:36 -0500

    handle dependsOn senario

--- JsonFSharp/Program.fs
+++ JsonFSharp/Program.fs
@@ -3,55 +3,37 @@ open System
 open canopy
 open runner
 open jsonSchema
+open System.Collections.Generic
 
+let data = System.IO.File.ReadAllText("data.json")
 
-let proximusTests = Simple.Parse(""" {
-"projects" : [
-    {
-        "name": "Proximus",
-        "environment" : {
-            "name": "QA"
-        },
-        "testCases" : [
-            {
-                "name": "SID Activation",
-                "steps": [
-                    {
-                        "action": "url",
-                        "target":"http://172.30.100.33:20000/Secure/Company/Home/CompanyHome.aspx?companyID=9"
-                    },
-                    {
-                        "action": "click",
-                        "target": "#ctl00_LeftNavTree > ul > li:nth-child(5) a:first"
-                    },
-                    {
-                        "action": "click",
-                        "target": "#ctl00_LeftNavTree > ul > li:nth-child(5) ul li:nth-child(2) a:first"
-                    },
-                    {
-                        "action": "write",
-                        "target": "#ctl00_RightPlaceHolder_ddlActivateTo",
-                        "value": "Active"
-                    }
-                ]
-            }
-        ]
-    }
-]
-} """)
-
+let proximusTests = Simple.Parse(data)
 
 start chrome
 
 "test 1" &&&& fun _->
-    let testSteps = proximusTests.Projects.[0].TestCases.[0].Steps
-    testSteps |> Seq.iter (fun(s) -> 
-                            match s.Action with
-                            |"url" -> url s.Target
-                            |"click" -> click s.Target
-                            |"write" -> s.Target << s.Value
-                            | _ -> ()
-                            )
+    let baseUrl = proximusTests.Projects.[0].Environment.BaseUrl
+    let runTests (tests:JsonProvider<"myJson.json">.Test[]) (parameters:Dictionary<string,string>) = 
+        let rec runTest (testId:string) (tests:JsonProvider<"myJson.json">.Test[]) (parameters:Dictionary<string,string>) = 
+            let test = tests |> Seq.filter (fun(t) -> t.TestId = testId) |> Seq.head
+            let n = test.TestId
+            if test.Parameters.Length > 0 && parameters.Count = 0 && not (test.DependsOn =  [||]) then
+                parameters.Add(test.Parameters.[0],"")
+                runTest test.DependsOn.[0] tests parameters
+            test.Steps |> Seq.iter (fun(s) -> 
+                match s.Action with
+                |"url" -> url (baseUrl + s.Target)
+                |"click" -> click s.Target
+                |"write" -> s.Target << s.Value
+                | _ -> ()
+                )
+        tests |> Seq.iter (fun(t) -> runTest t.TestId tests parameters) 
+    
+    let parameters = new Dictionary<string,string>()
+    let tcs = proximusTests.Projects.[0].Tests
+
+    runTests tcs parameters
+
 run()
     
 printfn "press [enter] to exit"
--- JsonFSharp/jsonSchema.fs
+++ JsonFSharp/jsonSchema.fs
@@ -2,25 +2,4 @@
 
 open FSharp.Data
 
-type Simple = JsonProvider<"""{
-    "projects" : [
-        {
-            "name": "Proximus",
-            "environment" : {
-                "name": "QA"
-            },
-            "testCases" : [
-                {
-                    "name": "Test Case Name",
-                    "steps": [
-                        {
-                            "action": "click",
-                            "target":"#button1",
-                            "value":"text"
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-}""">
+type Simple = JsonProvider<"myJson.json">
